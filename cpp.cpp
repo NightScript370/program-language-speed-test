18ad6a5cba7a430e7b34b006aa98b860cbec3e85
NightScript <maorninja@outlook.com>
2022-07-11 09:52:53 -0700

    Create cpp.cpp

---
+++ cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  const unsigned int MAX_INT = 4294967295;
+  unsigned int addition_opperation = 0;
+
+  while (addition_opperation < MAX_INT)
+    addition_opperation++;
+
+  std::cout << "Finished!" << std::endl;
+
+  return 0;
+}
